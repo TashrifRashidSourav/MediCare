b519a02a3536d4e5f2ca0a43b32102e7774e76f7
Md Tashrid Rashid Sourav <tashrifrashid.sourav@gmail.com>
2025-01-06 03:20:45 +0600

    update4

---
+++ html_Code/Manager/navbar.php
@@ -0,0 +1,11 @@
+<!-- Save this as navbar.php -->
+<nav class="navbar">
+    <div class="container">
+        <a href="patientDashboard.php" class="logo">Patient Dashboard</a>
+        <ul class="nav-links">
+            <li><a href="managerDashboard.php">Add Doctors</a></li>
+            <li><a href="managerAppointments.php">View Appointments</a></li>
+            <li><a href="../../html_Code/login.php?logout=true">Logout</a></li>
+        </ul>
+    </div>
+</nav>
