f8926213c4740baa2284a6551893133606c630fb
Md Tashrid Rashid Sourav <tashrifrashid.sourav@gmail.com>
2025-01-25 23:33:44 +0600

    update_7

--- html_Code/Doctor/navbar.php
+++ html_Code/Doctor/navbar.php
@@ -4,6 +4,7 @@
         <a href="doctorDashboard.php" class="logo">Patient Dashboard</a>
         <ul class="nav-links">
             <li><a href="drmyappoinment.php">Appointments</a></li>
+            <li><a href="doctor_chat.php">Discussion</a></li>
           
             <li><a href="login.html">Logout</a></li>
         </ul>
